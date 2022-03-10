990f2f0b9ec95ba17814c47c9bb13b13bcd620c3
Frangelicomk <mk3987@aol.com>
2022-03-10 20:03:59 +0000

    testing on mobiles...

--- assets/js/snakegame.js
+++ assets/js/snakegame.js
@@ -219,19 +219,19 @@ function joystickPlay(){
         var x = joy.GetX();
         var y = joy.GetY();
     
-        if (x <= -8 && x >= -100 ) {
+        if (x <= -30 && x >= -100 ) {
             if (snakeSpeed.x !== 1) {
                 snakeSpeed = { x: -1, y: 0 };
             }
-        } else if (y >= 8 && y <= 100) {
+        } else if (y >= 30 && y <= 100) {
             if (snakeSpeed.y !== 1) {
                 snakeSpeed = { x: 0, y: -1 };
             }
-        } else if (x >= 8 && x <= 100) {
+        } else if (x >= 30 && x <= 100) {
             if (snakeSpeed.x !== -1) {
                 snakeSpeed = { x: 1, y: 0 };
             }
-        } else if (y <= -8 && y >= -100) {
+        } else if (y <= -30 && y >= -100) {
             if (snakeSpeed.y !== -1) {
                 snakeSpeed = { x: 0, y: 1 };
             }
